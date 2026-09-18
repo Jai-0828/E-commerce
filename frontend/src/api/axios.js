bd8a0f3d5a6def6fdec58d2487e89fe159d69b1c
Jai Prakash Yadav <jaiy082528@gmail.com>
2026-09-18 08:12:13 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: https://e-commerce-65dg.onrender.com,
+  baseURL: "https://e-commerce-65dg.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
